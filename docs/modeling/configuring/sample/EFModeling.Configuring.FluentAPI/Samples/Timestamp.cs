4ff91f5d7617d31fc4c95fa48ad97948b2c54daa
Rowan Miller <mail@romiller.com>
2015-12-01 16:04:29 -0800

    :memo: Timestamp info for Fluent API

---
+++ docs/modeling/configuring/sample/EFModeling.Configuring.FluentAPI/Samples/Timestamp.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Data.Entity;
+
+namespace EFModeling.Configuring.FluentAPI.Samples.Timestamp
+{
+    class MyContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Blog>()
+                .Property(p => p.Timestamp)
+                .ValueGeneratedOnAddOrUpdate()
+                .IsConcurrencyToken();
+        }
+    }
+
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Url { get; set; }
+        public byte[] Timestamp { get; set; }
+    }
+}
