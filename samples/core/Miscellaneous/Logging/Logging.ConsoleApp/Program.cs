706e8a523c21cc52f7228b5742934b7996be2d49
Arthur Vickers <ajcvickers@hotmail.com>
2017-11-07 14:32:52 -0800

    Update logging documentation
    Part of https://github.com/aspnet/EntityFrameworkCore/issues/10211
    
    Shows how to do logging in a more 2.0-centric way, and emphasizes the risk of a memory leak if done wrong.

--- samples/core/Miscellaneous/Logging/Logging.ConsoleApp/Program.cs
+++ samples/core/Miscellaneous/Logging/Logging.ConsoleApp/Program.cs
@@ -1,23 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore.Infrastructure;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using System;
+﻿using System;
 
 namespace EFLogging.ConsoleApp
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            #region Sample
-            using (var db = new BloggingContext())
-            {
-                var serviceProvider = db.GetInfrastructure<IServiceProvider>();
-                var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
-                loggerFactory.AddProvider(new MyLoggerProvider());
-            }
-            #endregion
-
             using (var db = new BloggingContext())
             {
                 db.Database.EnsureCreated();
@@ -26,6 +14,21 @@ namespace EFLogging.ConsoleApp
             }
 
             using (var db = new BloggingContext())
+            {
+                foreach (var blog in db.Blogs)
+                {
+                    Console.WriteLine(blog.Url);
+                }
+            }
+
+            using (var db = new BloggingContextWithFiltering())
+            {
+                db.Database.EnsureCreated();
+                db.Blogs.Add(new Blog { Url = "http://sample.com" });
+                db.SaveChanges();
+            }
+
+            using (var db = new BloggingContextWithFiltering())
             {
                 foreach (var blog in db.Blogs)
                 {
