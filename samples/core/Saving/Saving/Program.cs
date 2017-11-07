84fa03630785e2879ada46f9b7ec9321ded5d65e
Diego Vega <diego.vega@microsoft.com>
2017-11-07 15:39:25 -0800

    Uncommenting samples in Program.Main

--- samples/core/Saving/Saving/Program.cs
+++ samples/core/Saving/Saving/Program.cs
@@ -4,15 +4,15 @@
     {
         static void Main(string[] args)
         {
-            //Basics.Sample.Run();
-            //RelatedData.Sample.Run();
+            Basics.Sample.Run();
+            RelatedData.Sample.Run();
             CascadeDelete.Sample.Run();
-            //Concurrency.Sample.Run();
-            //Transactions.ControllingTransaction.Sample.Run();
-            //Transactions.SharingTransaction.Sample.Run();
-            //Transactions.ExternalDbTransaction.Sample.Run();
-            //ExplicitValuesGenerateProperties.Sample.Run();
-            //Async.Sample.RunAsync().Wait();
+            Concurrency.Sample.Run();
+            Transactions.ControllingTransaction.Sample.Run();
+            Transactions.SharingTransaction.Sample.Run();
+            Transactions.ExternalDbTransaction.Sample.Run();
+            ExplicitValuesGenerateProperties.Sample.Run();
+            Async.Sample.RunAsync().Wait();
         }
     }
 }
