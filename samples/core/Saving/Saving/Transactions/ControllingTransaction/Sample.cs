96a733cca3500d024c019b801b5b7b654dcf709e
Simon Cropp <simon.cropp@gmail.com>
2019-07-16 09:45:10 +1000

    remove redundant catch

--- samples/core/Saving/Saving/Transactions/ControllingTransaction/Sample.cs
+++ samples/core/Saving/Saving/Transactions/ControllingTransaction/Sample.cs
@@ -19,26 +19,19 @@ namespace EFSaving.Transactions.ControllingTransaction
             {
                 using (var transaction = context.Database.BeginTransaction())
                 {
-                    try
-                    {
-                        context.Blogs.Add(new Blog { Url = "http://blogs.msdn.com/dotnet" });
-                        context.SaveChanges();
+                    context.Blogs.Add(new Blog { Url = "http://blogs.msdn.com/dotnet" });
+                    context.SaveChanges();
 
-                        context.Blogs.Add(new Blog { Url = "http://blogs.msdn.com/visualstudio" });
-                        context.SaveChanges();
+                    context.Blogs.Add(new Blog { Url = "http://blogs.msdn.com/visualstudio" });
+                    context.SaveChanges();
 
-                        var blogs = context.Blogs
-                            .OrderBy(b => b.Url)
-                            .ToList();
+                    var blogs = context.Blogs
+                        .OrderBy(b => b.Url)
+                        .ToList();
 
-                        // Commit transaction if all commands succeed, transaction will auto-rollback
-                        // when disposed if either commands fails
-                        transaction.Commit();
-                    }
-                    catch (Exception)
-                    {
-                        // TODO: Handle failure
-                    }
+                    // Commit transaction if all commands succeed, transaction will auto-rollback
+                    // when disposed if either commands fails
+                    transaction.Commit();
                 }
             }
             #endregion
--- samples/core/Saving/Saving/Transactions/SharingTransaction/Sample.cs
+++ samples/core/Saving/Saving/Transactions/SharingTransaction/Sample.cs
@@ -30,28 +30,21 @@ namespace EFSaving.Transactions.SharingTransaction
             {
                 using (var transaction = context1.Database.BeginTransaction())
                 {
-                    try
+                    context1.Blogs.Add(new Blog { Url = "http://blogs.msdn.com/dotnet" });
+                    context1.SaveChanges();
+
+                    using (var context2 = new BloggingContext(options))
                     {
-                        context1.Blogs.Add(new Blog { Url = "http://blogs.msdn.com/dotnet" });
-                        context1.SaveChanges();
+                        context2.Database.UseTransaction(transaction.GetDbTransaction());
 
-                        using (var context2 = new BloggingContext(options))
-                        {
-                            context2.Database.UseTransaction(transaction.GetDbTransaction());
-
-                            var blogs = context2.Blogs
-                                .OrderBy(b => b.Url)
-                                .ToList();
-                        }
-
-                        // Commit transaction if all commands succeed, transaction will auto-rollback
-                        // when disposed if either commands fails
-                        transaction.Commit();
+                        var blogs = context2.Blogs
+                            .OrderBy(b => b.Url)
+                            .ToList();
                     }
-                    catch (Exception)
-                    {
-                        // TODO: Handle failure
-                    }
+
+                    // Commit transaction if all commands succeed, transaction will auto-rollback
+                    // when disposed if either commands fails
+                    transaction.Commit();
                 }
             }
             #endregion
