e00d4986adc4d27d7a08d4c6affbe49de837a83c
Shay Rojansky <roji@roji.org>
2019-09-09 14:32:09 +0200

    Document C# 8 nullable reference types
    Closes #1654
    Closes #1655

---
+++ samples/core/Miscellaneous/NullableReferenceTypes/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace NullableReferenceTypes
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new NullableReferenceTypesContext())
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+
+                context.Add(new Customer("John", "Doe"));
+
+                context.Add(new Order
+                {
+                    ShippingAddress = new Address("London", "Downing"),
+                    Product = new Product("Cooking stove"),
+                    OptionalInfo = new OptionalOrderInfo("Some additional info")
+                    {
+                        ExtraAdditionalInfo = new ExtraOptionalOrderInfo("Some extra additional info")
+                    }
+                });
+
+                context.SaveChanges();
+            }
+
+            using (var context = new NullableReferenceTypesContext())
+            {
+                var john = context.Customers.First(c => c.FirstName == "John");
+                Console.WriteLine("John's last name: " + john.LastName);
+
+                var order = context.Orders
+                    .Include(o => o.OptionalInfo!)
+                        .ThenInclude(op => op.ExtraAdditionalInfo)
+                    .Single();
+
+                // The following would be a programming bug: we forgot to include ShippingAddress above. It would throw InvalidOperationException.
+                // Console.WriteLine(order.ShippingAddress.City);
+                // The following would be a programming bug: we forgot to include Product above; will throw NullReferenceException. It would throw NullReferenceException.
+                // Console.WriteLine(order.Product.Name);
+
+                Console.WriteLine(order.OptionalInfo!.ExtraAdditionalInfo!.SomeExtraAdditionalInfo);
+            }
+        }
+    }
+}
