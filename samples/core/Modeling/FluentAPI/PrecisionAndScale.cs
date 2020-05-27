d94fd79441a28278641ce51519c5bdbfb93677ce
lajones <lajones@microsoft.com>
2020-05-27 11:26:42 -0700

    Fix for issue 527 - Precision and Scale.

---
+++ samples/core/Modeling/FluentAPI/PrecisionAndScale.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace EFModeling.FluentAPI.PrecisionAndScale
+{
+    class MyContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+
+        #region PrecisionAndScale
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Blog>()
+                .Property(b => b.Score)
+                .HasPrecision(14, 2);
+
+            modelBuilder.Entity<Blog>()
+                .Property(b => b.LastUpdated)
+                .HasPrecision(3);
+        }
+        #endregion
+    }
+
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public decimal Score { get; set; }
+        public DateTime LastUpdated { get; set; }
+    }
+}
