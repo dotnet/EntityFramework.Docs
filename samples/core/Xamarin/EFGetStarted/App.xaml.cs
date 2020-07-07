cc0226035b53a879638e33234f2d53c537128302
Matt Soucoup <masoucou@microsoft.com>
2020-07-07 12:29:14 -0700

    finish xam article and code sample
    updating toc
    
    adding images and requested PR changes
    
    updating code from outside files and metadata
    
    fixing suggested changes

---
+++ samples/core/Xamarin/EFGetStarted/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EFGetStarted
+{
+    public partial class App : Application
+    {        
+        public App()
+        {
+            InitializeComponent();
+            
+            MainPage = new NavigationPage(new BlogsPage());
+        }
+
+        protected override void OnStart()
+        {
+            
+        }
+
+        protected override void OnSleep()
+        {
+            
+        }
+
+        protected override void OnResume()
+        {
+            
+        }
+    }
+}
---
+++ samples/core/Xamarin/EFGetStarted/Models/Post.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace EFGetStarted
+{
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public int BlogId { get; set; }
+        public Blog Blog { get; set; }
+    }
+}
