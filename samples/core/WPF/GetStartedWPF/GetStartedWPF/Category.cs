88e4a44c5b327a11ee229c6de62a1bb12fecb48c
Jeremy Likness <Jeremy.Likness@microsoft.com>
2020-07-25 12:56:01 -0700

    Get started with WPF (#2535)
    * Get started with WPF
    
    * Add full file path per @smitpatel feedback
    
    * Link to EF Core version
    
    * Updates re: @bricelam feedback.
    
    * Add note on property changes per @bricelam
    
    * Crop app screenshot
    
    * Fix interface link

---
+++ samples/core/WPF/GetStartedWPF/GetStartedWPF/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace GetStartedWPF
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Product>
+            Products
+        { get; private set; } =
+            new ObservableCollection<Product>();
+    }
+}
---
+++ samples/core/WPF/GetStartedWPF/GetStartedWPF/ProductContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GetStartedWPF
+{
+    public class ProductContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(
+            DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(
+                "Data Source=products.db");
+            optionsBuilder.UseLazyLoadingProxies();
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
