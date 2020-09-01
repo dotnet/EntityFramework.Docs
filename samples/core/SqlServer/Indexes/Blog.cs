119d7b8177f057ef217ef232c33dfbb025e9ba69
Shay Rojansky <roji@roji.org>
2020-09-01 18:22:59 +0300

    Add docs on SQL Server-specific index config
    Closes #2378
    Closes #878

---
+++ samples/core/SqlServer/Indexes/Blog.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SqlServer.Indexes
+{
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Url { get; set; }
+        public DateTime PublishedOn { get; set; }
+    }
+}
---
+++ samples/core/SqlServer/Indexes/IndexFillFactorContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SqlServer.Indexes
+{
+    public class IndexFillFactorContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+
+        #region IndexFillFactor
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Blog>().HasIndex(b => b.PublishedOn).HasFillFactor(10);
+        }
+        #endregion
+    }
+}
