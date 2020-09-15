70ff0e32dc29f0b381bbfb1048ea7f1c059d2ce7
AndriySvyryd <Andriy.Svyryd@microsoft.com>
2020-09-14 19:15:13 -0700

    Add a sample for the new Cosmos options
    Fixes #2428

---
+++ samples/core/Cosmos/ModelBuilding/OptionsContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Azure.Cosmos;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Net;
+
+namespace Cosmos.ModelBuilding
+{
+    public class OptionsContext : DbContext
+    {
+        #region Configuration
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseCosmos(
+                    "AccountEndpoint=https://localhost:8081/;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==",
+                    databaseName: "OptionsDB",
+                    options =>
+                    {
+                        options.ConnectionMode(ConnectionMode.Gateway);
+                        options.WebProxy(new WebProxy());
+                        options.LimitToEndpoint();
+                        options.Region(Regions.AustraliaCentral);
+                        options.GatewayModeMaxConnectionLimit(32);
+                        options.MaxRequestsPerTcpConnection(8);
+                        options.MaxTcpConnectionsPerEndpoint(16);
+                        options.IdleTcpConnectionTimeout(TimeSpan.FromMinutes(1));
+                        options.OpenTcpConnectionTimeout(TimeSpan.FromMinutes(1));
+                        options.RequestTimeout(TimeSpan.FromMinutes(1));
+                    });
+        #endregion
+    }
+}
