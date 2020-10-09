4a6dbe6276a7d76cfe730621d27c5763f0555f77
Arthur Vickers <ajcvickers@hotmail.com>
2020-10-09 09:58:30 -0700

    Docs for simple logging (#2740)
    Fixes #2085
    
    Note that this is the first part of several new pages/updates for logging and interception. For now, I'm linking to the new page from the old top-level logging. As more of this gets done that will gradually be replaced. I didn't add anything to the table of contents yet so as not to make this too confusing.

--- entity-framework/core/miscellaneous/logging.md
+++ entity-framework/core/miscellaneous/logging.md
@@ -2,13 +2,30 @@
 title: Logging - EF Core
 description: Configuring logging with Entity Framework Core
 author: ajcvickers
-ms.date: 10/27/2016
+ms.date: 10/06/2020
 uid: core/miscellaneous/logging
 ---
 # Logging
 
 > [!TIP]  
 > You can view this article's [sample](https://github.com/dotnet/EntityFramework.Docs/tree/master/samples/core/Miscellaneous/Logging) on GitHub.
+
+## Simple logging
+
+> [!NOTE]
+> This feature was added in EF Core 5.0.
+
+Entity Framework Core (EF Core) generates log messages for operations such as executing a query or saving changes to the database. These can be accessed from any type of application through use of [LogTo](https://github.com/dotnet/efcore/blob/ec3df8fd7e4ea4ebeebfa747619cef37b23ab2c6/src/EFCore/DbContextOptionsBuilder.cs#L135) <!-- Issue #2748 <xref:Microsoft.EntityFrameworkCore.DbContextOptionsBuilder.LogTo%2A> --> when [configuring a DbContext instance](xref:core/miscellaneous/configuring-dbcontext). This configuration is commonly done in an override of <xref:Microsoft.EntityFrameworkCore.DbContext.OnConfiguring%2A?displayProperty=nameWithType>. For example:
+
+<!--
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.LogTo(Console.WriteLine);
+-->
+[!code-csharp[LogToConsole](../../../samples/core/Miscellaneous/Logging/SimpleLogging/Program.cs?name=LogToConsole)]
+
+This concept is similar to <xref:System.Data.Entity.Database.Log?displayProperty=nameWithType> in EF6.
+
+See [Simple Logging](xref:core/miscellaneous/events/simple-logging) for more information.
 
 ## ASP.NET Core applications
 
---
+++ samples/core/Miscellaneous/Logging/SimpleLogging/Program.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Logging;
+
+public class Program
+{
+    public static void Main()
+    {
+        TestCode<LogToConsoleContext>();
+        TestCode<LogToDebugContext>();
+        TestCode<LogToFileContext>();
+        TestCode<InfoOnlyContext>();
+        TestCode<SensitiveDataLoggingContext>();
+        TestCode<EnableDetailedErrorsContext>();
+        TestCode<EventIdsContext>();
+        TestCode<DatabaseCategoryContext>();
+        TestCode<CustomFilterContext>();
+        TestCode<UtcContext>();
+        TestCode<SingleLineContext>();
+        TestCode<TerseLogsContext>();
+        TestDatabaseLog();
+        
+        static void TestCode<TContext>()
+            where TContext : BlogsContext, new()
+        {
+            using var context = new TContext();
+
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+        }
+        
+        static void TestDatabaseLog()
+        {
+            using var context = new DatabaseLogContext();
+
+            context.Database.EnsureDeleted();
+
+            context.Log = Console.WriteLine;
+            
+            context.Database.EnsureCreated();
+        }
+    }
+}
+
+public class Blog
+{
+    public int Id { get; set; }
+    public int Name { get; set; }
+}
+
+public abstract class BlogsContext : DbContext
+{
+    protected BlogsContext()
+        : base(new DbContextOptionsBuilder().UseSqlite("DataSource=test.db").Options)
+    {
+    }
+
+    public DbSet<Blog> Blogs { get; set; }
+}
+
+public class LogToConsoleContext : BlogsContext
+{
+    #region LogToConsole
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.LogTo(Console.WriteLine);
+    #endregion
+}
+
+public class LogToDebugContext : BlogsContext
+{
+    #region LogToDebug
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.LogTo(message => Debug.WriteLine(message));
+    #endregion
+}
+
+public class LogToFileContext : BlogsContext
+{
+    #region LogToFile
+    private readonly StreamWriter _logStream = new StreamWriter("mylog.txt", append: true); 
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.LogTo(_logStream.WriteLine);
+
+    public override void Dispose()
+    {
+        base.Dispose();
+        _logStream.Dispose();
+    }
+    
+    public override async ValueTask DisposeAsync()
+    {
+        await base.DisposeAsync();
+        await _logStream.DisposeAsync();
+    }
+    #endregion
+}
+
+public class InfoOnlyContext : BlogsContext
+{
+    #region InfoOnly
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.LogTo(Console.WriteLine, LogLevel.Information);
+    #endregion
+}
+
+public class SensitiveDataLoggingContext : BlogsContext
+{
+    #region SensitiveDataLogging
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder
+            .LogTo(Console.WriteLine)
+            .EnableSensitiveDataLogging();
+    #endregion
+}
+
+public class EnableDetailedErrorsContext : BlogsContext
+{
+    #region EnableDetailedErrors
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder
+            .LogTo(Console.WriteLine)
+            .EnableDetailedErrors();
+    #endregion
+}
+
+public class EventIdsContext : BlogsContext
+{
+    #region EventIds
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder
+            .LogTo(Console.WriteLine, new[] { CoreEventId.ContextDisposed, CoreEventId.ContextInitialized });
+    #endregion
+}
+
+public class DatabaseCategoryContext : BlogsContext
+{
+    #region DatabaseCategory
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder
+            .LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Name });
+    #endregion
+}
+
+public class CustomFilterContext : BlogsContext
+{
+    #region CustomFilter
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder
+            .LogTo(
+                Console.WriteLine,
+                (eventId, logLevel) => logLevel >= LogLevel.Information
+                                       || eventId == RelationalEventId.ConnectionOpened
+                                       || eventId == RelationalEventId.ConnectionClosed);
+    #endregion
+}
+
+public class UtcContext : BlogsContext
+{
+    #region Utc
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.LogTo(
+            Console.WriteLine,
+            LogLevel.Debug,
+            DbContextLoggerOptions.DefaultWithUtcTime);
+    #endregion
+}
+
+public class SingleLineContext : BlogsContext
+{
+    #region SingleLine
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.LogTo(
+            Console.WriteLine,
+            LogLevel.Debug,
+            DbContextLoggerOptions.DefaultWithLocalTime | DbContextLoggerOptions.SingleLine);
+    #endregion
+}
+
+public class TerseLogsContext : BlogsContext
+{
+    #region TerseLogs
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.LogTo(
+            Console.WriteLine,
+            LogLevel.Debug,
+            DbContextLoggerOptions.UtcTime | DbContextLoggerOptions.SingleLine);
+    #endregion
+}
+
+public class DatabaseLogContext : BlogsContext
+{
+    #region DatabaseLog
+    public Action<string> Log { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.LogTo(s => Log?.Invoke(s));
+    #endregion
+}
+
