6b9686be397d1744f7c4859c453085fc997f23e8
Shay Rojansky <roji@roji.org>
2020-10-15 23:50:16 +0300

    Tweak NRT guidance (#2778)
    Fixes #2529

--- samples/core/Miscellaneous/NullableReferenceTypes/Customer.cs
+++ samples/core/Miscellaneous/NullableReferenceTypes/Customer.cs
@@ -8,6 +8,8 @@
         public string LastName { get; set; }     // Required by convention
         public string? MiddleName { get; set; }  // Optional by convention
 
+        // Note the following use of constructor binding, which avoids compiled warnings
+        // for uninitialized non-nullable properties.
         public Customer(string firstName, string lastName, string? middleName = null)
         {
             FirstName = firstName;
---
+++ samples/core/Miscellaneous/NullableReferenceTypes/CustomerWithWarning.cs
@@ -0,0 +1,13 @@
+﻿#pragma warning disable CS8618
+
+namespace NullableReferenceTypes
+{
+    #region CustomerWithWarning
+    public class CustomerWithWarning
+    {
+        public int Id { get; set; }
+        // Generates CS8618, uninitialized non-nullable property:
+        public string Name { get; set; }
+    }
+    #endregion
+}
