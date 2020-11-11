ab6413a8c0ecf387622744a97e02e79dc1b06afe
Smit Patel <smitpatel@users.noreply.github.com>
2020-11-10 16:10:54 -0800

    Add doc for indexer property (#2860)
    Resolves #2018

---
+++ samples/core/Modeling/FluentAPI/IndexerProperty.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+namespace EFModeling.FluentAPI.IndexerProperty
+{
+    class MyContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+
+        #region ShadowProperty
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Blog>().IndexerProperty<DateTime>("LastUpdated");
+        }
+        #endregion
+    }
+
+    public class Blog
+    {
+        private readonly Dictionary<string, object>  _data = new Dictionary<string, object>();
+        public int BlogId { get; set; }
+        public object this[string key]
+        {
+            get => _data[key];
+            set => _data[key] = value;
+
+        }
+    }
+}
