d699c072eec7a0f08a01cbae609376d1b32e24c9
Shay Rojansky <roji@roji.org>
2021-02-22 18:43:44 +0100

    Fix getting started app (#3079)
    Fixes #3078

--- samples/core/GetStarted/Model.cs
+++ samples/core/GetStarted/Model.cs
@@ -8,8 +8,10 @@ namespace EFGetStarted
         public DbSet<Blog> Blogs { get; set; }
         public DbSet<Post> Posts { get; set; }
 
+        // The following configures EF to create a Sqlite database file as `C:\blogging.db`.
+        // For Mac or Linux, change this to `/tmp/blogging.db` or any other absolute path.
         protected override void OnConfiguring(DbContextOptionsBuilder options)
-            => options.UseSqlite("Data Source=blogging.db");
+            => options.UseSqlite(@"Data Source=C:\blogging.db");
     }
 
     public class Blog
