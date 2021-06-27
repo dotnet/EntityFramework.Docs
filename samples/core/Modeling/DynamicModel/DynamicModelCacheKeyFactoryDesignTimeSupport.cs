26de24ddfa53c5c353a1662e187f1c3b6359700a
Joel Mandell <joelmandell@gmail.com>
2021-06-27 13:20:29 +0200

    Added missing required Create method.

--- samples/core/Modeling/DynamicModel/DynamicModelCacheKeyFactoryDesignTimeSupport.cs
+++ samples/core/Modeling/DynamicModel/DynamicModelCacheKeyFactoryDesignTimeSupport.cs
@@ -3,13 +3,16 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 
 namespace EFModeling.DynamicModel
 {
-    #region DynamicModel
+    #region DynamicModelDesignTimeSupport
     public class DynamicModelCacheKeyFactory : IModelCacheKeyFactory
     {
         public object Create(DbContext context)
             => context is DynamicContext dynamicContext
                 ? (context.GetType(), dynamicContext.UseIntProperty)
                 : (object)context.GetType();
+
+        public object Create(DbContext context)
+            => Create(context, false);
     }
     #endregion
 }
