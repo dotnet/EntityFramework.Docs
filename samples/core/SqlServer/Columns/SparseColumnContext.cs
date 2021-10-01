c7e69f060f5b0deeccdd1440de929027882ff344
Shay Rojansky <roji@roji.org>
2021-10-01 18:04:16 +0200

    Document SQL Server sparse columns (#3456)
    Closes #3017

---
+++ samples/core/SqlServer/Columns/SparseColumnContext.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace SqlServer.Columns
+{
+    public class SparseColumnContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<RareBlog> RareBlogs { get; set; }
+
+        #region SparseColumn
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<RareBlog>()
+                .Property(b => b.RareProperty)
+                .IsSparse();
+        }
+        #endregion
+    }
+}
