a356305ae97405df37ef076caaaabafbc41be106
Smit Patel <smitpatel@users.noreply.github.com>
2021-11-11 13:43:11 -0800

    Document AutoInclude (#3550)
    Resolves #3193

---
+++ samples/core/Querying/RelatedData/Theme.cs
@@ -0,0 +1,10 @@
+﻿namespace EFQuerying.RelatedData
+{
+    public class Theme
+    {
+        public int Id { get; set; }
+
+        public int ColorSchemeId { get; set; }
+        public ColorScheme ColorScheme { get; set; }
+    }
+}
