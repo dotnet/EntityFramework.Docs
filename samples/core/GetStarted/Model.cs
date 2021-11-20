c1b012edbbbdb16dffa88bba495e7cd1ee1f56a2
Petr Onderka <gsvick@gmail.com>
2021-11-20 18:53:53 +0100

    Simplify code (#3574)
    * Simplify code by using Path.Join
    * Use recent C# features

--- samples/core/GetStarted/Model.cs
+++ samples/core/GetStarted/Model.cs
@@ -9,13 +9,13 @@ namespace EFGetStarted
         public DbSet<Blog> Blogs { get; set; }
         public DbSet<Post> Posts { get; set; }
 
-        public string DbPath { get; private set; }
+        public string DbPath { get; }
 
         public BloggingContext()
         {
             var folder = Environment.SpecialFolder.LocalApplicationData;
             var path = Environment.GetFolderPath(folder);
-            DbPath = $"{path}{System.IO.Path.DirectorySeparatorChar}blogging.db";
+            DbPath = System.IO.Path.Join(path, "blogging.db");
         }
 
         // The following configures EF to create a Sqlite database file in the
@@ -29,7 +29,7 @@ namespace EFGetStarted
         public int BlogId { get; set; }
         public string Url { get; set; }
 
-        public List<Post> Posts { get; } = new List<Post>();
+        public List<Post> Posts { get; } = new();
     }
 
     public class Post
