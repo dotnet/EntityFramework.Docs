79f1dcbc1e445b3760388e0ccb6ce36953b0b43e
Petr Onderka <gsvick@gmail.com>
2021-11-20 10:03:55 +0100

    Use recent C# features

--- samples/core/GetStarted/Model.cs
+++ samples/core/GetStarted/Model.cs
@@ -9,7 +9,7 @@ namespace EFGetStarted
         public DbSet<Blog> Blogs { get; set; }
         public DbSet<Post> Posts { get; set; }
 
-        public string DbPath { get; private set; }
+        public string DbPath { get; }
 
         public BloggingContext()
         {
@@ -29,7 +29,7 @@ namespace EFGetStarted
         public int BlogId { get; set; }
         public string Url { get; set; }
 
-        public List<Post> Posts { get; } = new List<Post>();
+        public List<Post> Posts { get; } = new();
     }
 
     public class Post
