b51f25f35f584fff1ce35894d33b531481b8aded
Jeremy Likness <jeremy@jeremylikness.com>
2022-01-11 11:13:22 -0800

    Evolving samples

--- samples/core/Miscellaneous/Multitenancy/MultiDb/Program.cs
+++ samples/core/Miscellaneous/Multitenancy/MultiDb/Program.cs
@@ -1,15 +1,32 @@
+using Common;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-using MultiDb.Data;
+using Microsoft.EntityFrameworkCore;
+using MultiDb;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddScoped<ITenantService, TenantService>();
+builder.Services.AddDbContextFactory<ContactContext>(opt => { }, ServiceLifetime.Scoped);
 
 var app = builder.Build();
+
+// just for demo to avoid migrations, don't do this in production
+var provider = app.Services.CreateScope().ServiceProvider;
+var tenantService = provider.GetService<ITenantService>();
+var factory = provider.GetService<IDbContextFactory<ContactContext>>();
+if (tenantService != null && factory != null)
+{
+    foreach (var tenant in tenantService.GetTenants())
+    {
+        tenantService.SetTenant(tenant);
+        using var ctx = factory.CreateDbContext();
+        ctx.CheckAndSeed();
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
