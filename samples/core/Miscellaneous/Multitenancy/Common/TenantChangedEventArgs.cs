8113036069177e7e2ef51c16ecf5cffc85a6dabc
Jeremy Likness <jeremy@jeremylikness.com>
2022-01-11 09:06:27 -0800

    Build web-based sample

---
+++ samples/core/Miscellaneous/Multitenancy/Common/TenantChangedEventArgs.cs
@@ -0,0 +1,15 @@
+﻿namespace Common
+{
+    public class TenantChangedEventArgs : EventArgs
+    {
+        public TenantChangedEventArgs(string? oldTenant, string newTenant)
+        {
+            OldTenant = oldTenant;
+            NewTenant = newTenant;
+        }
+
+        public string? OldTenant { get; private set; }
+
+        public string NewTenant { get; private set; }
+    }
+}
