b50ca2240084ea8e343e260af39153b59ebd5796
Shay Rojansky <roji@roji.org>
2022-07-21 19:17:48 +0200

    Document index sort ordering
    Closes #3691

---
+++ samples/core/Modeling/IndexesAndConstraints/DataAnnotations/IndexDescendingAscending.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFModeling.IndexesAndConstraints.DataAnnotations.IndexDescendingAscending;
+
+internal class MyContext : DbContext
+{
+    public DbSet<Blog> Blogs { get; set; }
+}
+
+#region IndexDescendingAscending
+[Index(nameof(Url), nameof(Rating), IsDescending = new[] { false, true })]
+public class Blog
+{
+    public int BlogId { get; set; }
+    public string Url { get; set; }
+    public int Rating { get; set; }
+}
+#endregion
