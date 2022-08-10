e951e15a52c22027bb9cd8c7c0f9dc191b3ccb9e
Arthur Vickers <ajcvickers@hotmail.com>
2022-08-10 19:43:12 +0100

    Walkthrough for Get Started with WinForms (#3968)
    Co-authored-by: Klaus Löffelmann <9663150+KlausLoeffelmann@users.noreply.github.com>

---
+++ samples/core/WinForms/GetStartedWinForms/GetStartedWinForms/Category.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace GetStartedWinForms;
+
+public class Category
+{
+    public int CategoryId { get; set; }
+
+    public string? Name { get; set; }
+
+    public virtual ObservableCollectionListSource<Product> Products { get; } = new();
+}
