5d71e3aeb5f2d8de1993328bd364494aeca6d8bd
Shay Rojansky <roji@roji.org>
2023-06-20 17:17:09 +0200

    Bring back file

---
+++ samples/core/Miscellaneous/NullableReferenceTypes/NullableReferenceTypesContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NullableReferenceTypes
+{
+    #region Context
+    public class NullableReferenceTypesContext : DbContext
+    {
+        public DbSet<Customer> Customers => Set<Customer>();
+        public DbSet<Order> Orders => Set<Order>();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder
+                .UseSqlServer(
+                    @"Server=(localdb)\mssqllocaldb;Database=EFNullableReferenceTypes;Trusted_Connection=True");
+    }
+    #endregion
+}
