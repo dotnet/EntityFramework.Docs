c18447917e5f310b00fcd191b118c19a8db53a76
Dick Baker <dickbaker@totalise.co.uk>
2024-01-18 17:40:44 +0000

    COB117

---
+++ samples/core/Miscellaneous/CompiledModels/Blog0001.cs
@@ -0,0 +1,25 @@
+﻿namespace CompiledModelTest;
+
+public class Blog0001 : BlogBase<Post0001, Tag0001>
+{
+}
+
+public class Blog0002 : BlogBase<Post0002, Tag0002>
+{
+}
+
+public class Blog0003 : BlogBase<Post0003, Tag0003>
+{
+}
+
+public class Blog0004 : BlogBase<Post0004, Tag0004>
+{
+}
+
+public class Blog0005 : BlogBase<Post0005, Tag0005>
+{
+}
+
+public class Blog0006 : BlogBase<Post0006, Tag0006>
+{
+}
