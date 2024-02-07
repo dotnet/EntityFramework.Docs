7c4e4d65afa7c7a758aeccb4d83b6929a4d26c34
Arthur Vickers <ajcvickers@hotmail.com>
2024-02-07 17:27:49 +0000

    What's new for EF9 Preview 1 (#4642)
    * What's new for EF9 Preview 1
    
    * Update entity-framework/core/what-is-new/ef-core-9.0/whatsnew.md
    
    Co-authored-by: Andriy Svyryd <AndriySvyryd@users.noreply.github.com>
    
    * Update entity-framework/core/what-is-new/ef-core-9.0/whatsnew.md
    
    Co-authored-by: Andriy Svyryd <AndriySvyryd@users.noreply.github.com>
    
    * Updates
    
    * Fix warnings
    
    * Add note about now.
    
    ---------
    
    Co-authored-by: Andriy Svyryd <AndriySvyryd@users.noreply.github.com>

---
+++ samples/core/Miscellaneous/NewInEFCore9/Program.cs
@@ -0,0 +1,22 @@
+﻿using NewInEfCore9;
+
+public class Program
+{
+    public static async Task Main()
+    {
+        await QuerySample.Query_improvements_in_EF9();
+        await QuerySample.Query_improvements_in_EF9_on_SQLite();
+
+        // Note that SQL Server 2022 is required for Greater and Least queries.
+        // await LeastGreatestSample.Queries_using_Least_and_Greatest();
+        await LeastGreatestSample.Queries_using_Least_and_Greatest_on_SQLite();
+
+        await ModelBuildingSample.Model_building_enhancements_in_EF9();
+
+        await JsonColumnsSample.Columns_from_JSON_are_pruned_when_needed();
+        await JsonColumnsSample.Columns_from_JSON_are_pruned_when_needed_on_SQLite();
+
+        await ExecuteUpdateSample.ExecuteUpdate_for_complex_type_instances();
+        await ExecuteUpdateSample.ExecuteUpdate_for_complex_type_instances_on_SQLite();
+    }
+}
---
+++ samples/core/Miscellaneous/NewInEFCore9/QuerySample.cs
@@ -0,0 +1,76 @@
+﻿namespace NewInEfCore9;
+
+public static class QuerySample
+{
+    public static Task Query_improvements_in_EF9()
+    {
+        PrintSampleName();
+        return QueryTest<JsonBlogsContext>();
+    }
+
+    public static Task Query_improvements_in_EF9_on_SQLite()
+    {
+        PrintSampleName();
+        return QueryTest<JsonBlogsContextSqlite>();
+    }
+
+    private static async Task QueryTest<TContext>()
+        where TContext : BlogsContext, new()
+    {
+        await using var context = new TContext();
+        await context.Database.EnsureDeletedAsync();
+        await context.Database.EnsureCreatedAsync();
+        await context.Seed();
+
+        context.LoggingEnabled = true;
+        context.ChangeTracker.Clear();
+
+        Console.WriteLine();
+        Console.WriteLine("Default parameterization:");
+        Console.WriteLine();
+
+        _ = await GetPosts(1);
+
+        #region DefaultParameterization
+        async Task<List<Post>> GetPosts(int id)
+            => await context.Posts
+                .Where(
+                    e => e.Title == ".NET Blog" && e.Id == id)
+                .ToListAsync();
+        #endregion
+
+        Console.WriteLine();
+        Console.WriteLine("Force parameterization of a constant:");
+        Console.WriteLine();
+
+        _ = await GetPostsForceParameter(1);
+
+        #region ForceParameter
+        async Task<List<Post>> GetPostsForceParameter(int id)
+            => await context.Posts
+                .Where(
+                    e => e.Title == EF.Parameter(".NET Blog") && e.Id == id)
+                .ToListAsync();
+        #endregion
+
+        Console.WriteLine();
+        Console.WriteLine("Force constant:");
+        Console.WriteLine();
+
+        _ = await GetPostsForceConstant(1);
+
+        #region ForceConstant
+        async Task<List<Post>> GetPostsForceConstant(int id)
+            => await context.Posts
+                .Where(
+                    e => e.Title == ".NET Blog" && e.Id == EF.Constant(id))
+                .ToListAsync();
+        #endregion
+    }
+
+    private static void PrintSampleName([CallerMemberName] string? methodName = null)
+    {
+        Console.WriteLine($">>>> Sample: {methodName}");
+        Console.WriteLine();
+    }
+}
