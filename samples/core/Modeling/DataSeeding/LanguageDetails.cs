bdbdd8ef2486a2fc99e4065138638063d2f683f1
maumar <maumar@microsoft.com>
2024-10-13 21:56:07 -0700

    whats new - seeding
    - rename old seeding (i.e. HasData) to managed data (managing data)
    - modifying HasData sample so that it adds static data (which is how we want people to use HasData)
    - add example of HasData for Many-to-Many
    
    Fixes #2879

---
+++ samples/core/Modeling/DataSeeding/LanguageDetails.cs
@@ -0,0 +1,9 @@
+﻿namespace EFModeling.DataSeeding;
+
+public class LanguageDetails
+{
+    public bool Phonetic { get; set; }
+    public bool Tonal { get; set; }
+    public int PhonemesCount { get; set; }
+}
+
