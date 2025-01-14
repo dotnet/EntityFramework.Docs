8ce457428740320ac8b37d52282336ce780a8a9b
Shay Rojansky <roji@roji.org>
2025-01-14 12:59:23 +0100

    Remove unused SqlQueries samples

--- samples/core/Miscellaneous/ConfiguringDbContext/WithContextFactory/MyController.cs
+++ samples/core/Miscellaneous/ConfiguringDbContext/WithContextFactory/MyController.cs
@@ -3,6 +3,8 @@ using WebApp;
 
 namespace WithContextFactory;
 
+#pragma warning disable CS1998 // This async method lacks 'await' operators and will run synchronously.
+
 public class MyController
 {
     #region Construct
