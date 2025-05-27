7fe5bddced6f357f944acb368470d922eb02b49d
Andriy Svyryd <Andriy.Svyryd@microsoft.com>
2025-05-27 15:49:54 -0700

    Also update the default value docs

--- samples/core/Modeling/GeneratedProperties/FluentAPI/DefaultValueSql.cs
+++ samples/core/Modeling/GeneratedProperties/FluentAPI/DefaultValueSql.cs
@@ -7,14 +7,20 @@ internal class MyContext : DbContext
 {
     public DbSet<Blog> Blogs { get; set; }
 
-    #region DefaultValueSql
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        #region DefaultValueSql
         modelBuilder.Entity<Blog>()
             .Property(b => b.Created)
             .HasDefaultValueSql("getdate()");
+        #endregion
+
+        #region DefaultValueSqlNamed
+        modelBuilder.Entity<Blog>()
+            .Property(b => b.Created)
+            .HasDefaultValueSql("getdate()" , "DF_Blog_IsActive");
+        #endregion
     }
-    #endregion
 }
 
 public class Blog
